7b2b495a94d2227039cf99763f69d292267d59f4
KODEM AJAY RAGHAVA <154720431+ajayraghava03@users.noreply.github.com>
2026-06-09 22:49:25 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,40 @@
+name = "Python"
+
+# Concatenation
+print(name + " Programming")
+
+# Repetition
+print(name * 2)
+
+# Indexing
+print(name[0])
+
+# Slicing
+print(name[1:4])
+
+# Length
+print(len(name))
+
+# Membership
+print("P" in name)
+
+# Uppercase
+print(name.upper())
+
+
+# Implicit Conversion
+a = 10
+b = 5.5
+
+c = a + b
+
+print(c)
+print(type(c))
+
+# Explicit Conversion
+x = "100"
+
+x = int(x)
+
+print(x)
+print(type(x))
